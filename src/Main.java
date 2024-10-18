e0b809de1fd7268df78e700dfef4f79d11929e74
Sabrina <sabis476@gmail.com>
2024-10-18 13:24:36 -0300

    estou tratando uma exception de negocio

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         // Entrada de dados
         int a = 4;
         int b = 0;
@@ -9,6 +9,11 @@ public class Main {
 
         } catch (Exception e) {
             System.out.println("Erro!");
+        }
+
+        if (a == 4 ){
+            System.out.println(" 4 nao pode ");
+            throw new Exception(" exception customizada ");
         }
 
         System.out.println(resultado);
